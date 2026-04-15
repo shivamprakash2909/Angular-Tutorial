0a15527a33442f41a805382327b0b6ae7ebc7f15
Shivam Prakash <shivamprakash444@gmail.com>
2026-04-16 03:56:28 +0530

    proprty-binding completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular_tutoriAL');
+  protected readonly title = signal('angular_tutorial');
 }
