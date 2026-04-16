f8b5f95ca34aa44d132e1e1177652e2f2288a11a
Shivam Prakash <shivamprakash444@gmail.com>
2026-04-17 02:36:29 +0530

    Data-types completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular_tutoriAL');
+
+  value: string = 'Shivam';
+  value2: number = 10;
+  istrue: boolean = true;
+
+  sum(a: number, b: number): number {
+    console.log(a + b)
+    return a + b;
+  }
 }
