e281298bb2388d1b42d12b9cb9cdc905feed19ea
Shivam Prakash <shivamprakash444@gmail.com>
2026-04-20 03:05:00 +0530

    click-events completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular_tutoriAL');
+  onInput(event: any) {
+    console.log("Clicked", event);
+    console.log(event.target.value)
+  }
+  onKeyUp(event: any) {
+    console.log("Key up event", event.key)
+  }
+  onHover(event: any) {
+    console.log("Hover event", event)
+  }
+  onLeave(event: any) {
+    console.log("Leave event", event)
+  }
+  onBlur(event: any) {
+    console.log("Blur event", event)
+  }
+  onFocus(event: any) {
+    console.log("Focus event", event)
+  }
+
 }
