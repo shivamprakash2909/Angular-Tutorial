0722e15da1b49f67cff8937997a9eb95e004c66c
Shivam Prakash <shivamprakash444@gmail.com>
2026-04-22 01:30:40 +0530

    signaal-datatypes, interfaces completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { count } from 'rxjs/internal/operators/count';
+
+interface User {
+  name: string;
+  age: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -9,4 +15,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular_tutoriAL');
+  count = signal<number>(0);
+  name = signal<string>("Angular");
+  isAdmin = signal<boolean>(true);
+  user = signal<{ name: string, age: number }>({ name: "Shivam", age: 20 });
+  user1 = signal<User>({ name: "Shivam", age: 22 });
+  numbers = signal<number[]>([1, 3, 4, 5, 6])
+  updateCount() {
+    this.count.update(c => c + 1);
+    // this.count.set(v)
+  }
 }
