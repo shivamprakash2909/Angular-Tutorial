12ae3a09ecaeb500dc111de92329dd4b2144792f
Shivam Prakash <shivamprakash444@gmail.com>
2026-04-22 01:49:08 +0530

    computed-signal completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular_tutoriAL');
+  count = signal<number>(10);
+  doubleCount = computed<number>(() => this.count() * 2);
+  tripleCount = computed<number>(() => this.count() * 3)
+  increament() {
+    this.count.update(c => c + 1)
+  }
+  firstName = signal<string>('Shivam');
+  lastName = signal<string>('Prakash');
+  fullName = computed<string>(() => this.firstName() + ' ' + this.lastName())
+  prices = signal<number[]>([10, 20, 30, 40, 50, 60]);
+  cartValue = computed<number>(() => { return this.prices().reduce((curr, acc) => curr + acc, 0) });
+
 }
