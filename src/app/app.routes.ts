ce915ac02b516f9a08898ffdca0da000653e09e1
Shivam Prakash <shivamprakash444@gmail.com>
2026-06-01 03:16:03 +0530

    nested-routing completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard'
+import { Setting } from './dashboard/setting/setting';
+import { Profile } from './dashboard/profile/profile';
+export const routes: Routes = [
+    {
+        path: 'dashboard', component: Dashboard, children: [
+            { path: 'setting', component: Setting },
+            { path: 'profile', component: Profile }
+        ]
+    },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
 
-export const routes: Routes = [];
+
+];
---
+++ src/app/dashboard/dashboard.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard { }
