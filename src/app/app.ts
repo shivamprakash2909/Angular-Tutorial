c43d0df6f4b280aa2ddb54bac7002c6dd1f77e06
Shivam Prakash <shivamprakash444@gmail.com>
2026-06-01 00:07:29 +0530

    effect completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular_tutoriAL');
+  isDarkMode = signal(false);
+  constructor() {
+    effect(() => {
+      if (this.isDarkMode()) {
+        document.body.style.backgroundColor = 'black'
+        document.body.style.color = 'white'
+      } else {
+        document.body.style.backgroundColor = 'white'
+        document.body.style.color = 'black'
+      }
+    }
+    )
+  }
+  toggle() {
+    this.isDarkMode.update(val => !val);
+  }
 }
