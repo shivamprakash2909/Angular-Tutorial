0533d35dc3ec0350be3cdeb1c547f79783d7ceed
Shivam Prakash <shivamprakash444@gmail.com>
2026-06-01 00:00:26 +0530

    for loops conmpleted

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular_tutoriAL');
+  items: string[] = ['Item1', 'Item2', 'Item3', 'Item4']
+  users = [
+    { id: 1, name: "Shivam1" },
+    { id: 2, name: "Shivam2" },
+    { id: 3, name: "Shivam3" },
+    { id: 4, name: "Shivam4" },
+    { id: 5, name: "Shivam5" }
+  ]
 }
+
