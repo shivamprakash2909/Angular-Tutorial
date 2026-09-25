ed2c20ee98cfb8d61a0a6b1fa14107c0512bc57c
Shivam Prakash <shivamprakash444@gmail.com>
2026-09-25 20:32:34 +0530

    minor changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,17 +3,17 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular_tutoriAL');
   name = 'Angular Tutorial';
-  username = 'Shivamprakash';
+  username = 'Shivam prakash';
   isAdmin = true;
   title2 = signal('angular signal')
-  count = 100;
+  count = 100.098;
   getUser() {
     return this.username;
   }
